2599a26ce2f9fadd47f66e21cf9d477f5d909517
Daniel Martin <etd-github@nomejortu.com>
2015-02-18 11:04:57 +0100

    New dynamic pages for each add-on

--- config.rb
+++ config.rb
@@ -36,6 +36,22 @@ set :markdown_engine, :redcarpet
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+# Add-on dynamic pages
+data.tools.each do |addon_data|
+  proxy "/addons/#{addon_data[:page]}.html", "/addons/addon.html", ignore: true, locals: addon_data
+end
+
+# Hook ready, because the sitemap has definitely been built at this point
+ready do
+  data.tools.each do |tool_data|
+    resource = sitemap.find_resource_by_path "/addons/#{tool_data[:page]}.html"
+    resource.add_metadata page: {
+      title: "#{tool_data['title']} add-on | Dradis Framework",
+      description: tool_data['description']
+    }
+  end
+end
+
 ###
 # Helpers
 ###
