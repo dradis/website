cd64606a1fb70f6d3a75effec2d7db0cd5acc7a0
Daniel Martin <etd-github@nomejortu.com>
2015-11-10 11:00:44 +0100

    Config - add missing page redirects

--- config.rb
+++ config.rb
@@ -36,6 +36,10 @@ set :markdown_engine, :redcarpet
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+# Redirects
+redirect 'server_plugins.html', to: '/addons/'
+redirect 'subversion.html', to: '/download.html'
+
 # Add-on dynamic pages
 data.tools.each do |addon_data|
   proxy "/addons/#{addon_data[:page]}.html", "/addons/addon.html", ignore: true, locals: addon_data
